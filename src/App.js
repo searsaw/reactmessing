d1ed6df5b8ee2ca20d6b521854bdd3183b05b53b
Jake Boyles <jake@jibdesigns.com>
2016-08-08 12:08:25 -0400

    urg

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ var Main = React.createClass({
   },
 
   componentDidMount: function() {
-    console.log("Mounted");
     this.serverRequest = $.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.props.location}&mode=json&units=imperial&cnt=7&APPID=7cf16558d759d14815306832bd7341e2`, (result) => {
       this.setState({
         data: result.list,
@@ -23,9 +22,7 @@ var Main = React.createClass({
 
   render: function(){
       return (
-        <div>
           <Day data={this.state.data} />
-        </div>
       );
   }
 });
